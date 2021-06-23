28b3c11dedb6c1081a744c21961eb29832c33575
artsing <artsing@yeah.net>
2021-06-23 19:21:03 +0800

    driver for bochs

--- kernel/lbfvideo.c
+++ kernel/lbfvideo.c
@@ -5,10 +5,80 @@
 #include "mmu.h"
 #include "pci.h"
 
+static int PREFERRED_W = 1440;
+static int PREFERRED_H = 900;
+#define PREFERRED_VY 4096
+#define PREFERRED_B 32
+
+
+#define BOCHS_MMIO_ID       0x00
+#define BOCHS_MMIO_FBWIDTH  0x02
+#define BOCHS_MMIO_FBHEIGHT 0x04
+#define BOCHS_MMIO_BPP      0x06
+#define BOCHS_MMIO_ENABLED  0x08
+#define BOCHS_MMIO_VIRTX    0x0c
+#define BOCHS_MMIO_VIRTY    0x0e
+
+/* Exported to other modules */
+uint16_t lfb_resolution_x = 0;
+uint16_t lfb_resolution_y = 0;
+uint16_t lfb_resolution_b = 0;
+uint32_t lfb_resolution_s = 0;
+uint8_t * lfb_vid_memory = (uint8_t *)0xE0000000;
+size_t lfb_memsize = 0xFF0000;
+const char * lfb_driver_name = "bochs";
+
+uintptr_t lfb_bochs_mmio = 0;
+
+static void bochs_mmio_out(int off, uint16_t val) {
+	*(volatile uint16_t*)(lfb_bochs_mmio + 0x500 + off) = val;
+}
+
+static uint16_t bochs_mmio_in(int off) {
+	return *(volatile uint16_t*)(lfb_bochs_mmio + 0x500 + off);
+}
+
+static void bochs_set_resolution(uint16_t x, uint16_t y) {
+	bochs_mmio_out(BOCHS_MMIO_ENABLED,  0);
+	bochs_mmio_out(BOCHS_MMIO_FBWIDTH,  x);
+	bochs_mmio_out(BOCHS_MMIO_FBHEIGHT, y);
+	bochs_mmio_out(BOCHS_MMIO_BPP, PREFERRED_B);
+	bochs_mmio_out(BOCHS_MMIO_VIRTX, x * (PREFERRED_B  / 8));
+	bochs_mmio_out(BOCHS_MMIO_VIRTY, y);
+	bochs_mmio_out(BOCHS_MMIO_ENABLED,  0x41);
+
+    *(volatile uint8_t*)(lfb_bochs_mmio + 0x400) = 0x20;
+
+	lfb_resolution_x = bochs_mmio_in(BOCHS_MMIO_FBWIDTH);
+	lfb_resolution_y = bochs_mmio_in(BOCHS_MMIO_FBHEIGHT);
+	lfb_resolution_b = bochs_mmio_in(BOCHS_MMIO_BPP);
+	lfb_resolution_s = lfb_resolution_x * 4;
+
+    cprintf("[LBF][bochs_set_resolution] x = %d y = %d.\n", lfb_resolution_x, lfb_resolution_y);
+}
+
 int
 lbf_video_init(struct pci_func *pcif)
 {
-    cprintf("[lbfvideo] dev_id = %02x dev_class = %02x.\n", pcif->dev_id, pcif->dev_class);
+    cprintf("[LBF][lbf_vide_init] dev_id = %02x dev_class = %02x.\n",
+            pcif->dev_id, pcif->dev_class);
+    pci_func_enable(pcif);
+
+    lfb_vid_memory = (uint8_t *) pcif->reg_base[0];
+    lfb_bochs_mmio = (uintptr_t) pcif->reg_base[2];
+    lfb_memsize = pcif->reg_size[0];
+
+    cprintf("[LBF][lbf_video_init] lfb_vid_memory = %02x \t lfb_bochs_mmio = %02x \t lfb_memsize = %02x.\n",
+            lfb_vid_memory, lfb_bochs_mmio, lfb_memsize);
+
+    uint16_t i = bochs_mmio_in(BOCHS_MMIO_ID);
+	if (i < 0xB0C0 || i > 0xB0C6) {
+        cprintf("[LBF][lbf_video_init] error unsupported bochs device.");
+        return -1;
+    }
+
+    bochs_mmio_out(BOCHS_MMIO_ID, 0xB0C4);
+    bochs_set_resolution(PREFERRED_W, PREFERRED_H);
 
     return 0;
 }

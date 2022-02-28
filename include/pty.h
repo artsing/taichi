40a41d1b14afb41b93cee198d3f350eb26c41693
artsing <artsing@yeah.net>
2022-02-28 23:54:49 +0800

    fix bugs

--- include/pty.h
+++ include/pty.h
@@ -1,16 +1,18 @@
 #ifndef _PTY_H_
 #define _PTY_H_
 
+typedef struct master_pty_s master_pty;
+typedef struct slave_pty_s slave_pty;
 
-typedef struct master_pty_s {
+struct master_pty_s {
     int index;
     char ptsname[20];
-    struct slave_pty_s *pts;
-} master_pty;
+    slave_pty *pts;
+};
 
-typedef struct slave_pty_s {
+struct slave_pty_s {
     master_pty *ptm;
-} slave_pty;
+};
 
 
 #endif // _PTY_H_

684ffe03be3bc30c3879a19e3a37c660cd937745
artsing <artsing@yeah.net>
2022-07-06 00:05:39 +0800

    dump ext2 group desc

--- boot/test.c
+++ boot/test.c
@@ -6,9 +6,9 @@
 struct ext2_super_block ext2_sb;
 struct ext2_group_desc ext2_gd;
 
-void dumpUUID(__u8* uuid);
+void dumpUUID(const char*, __u8*);
 void dumpSuperBlock(struct ext2_super_block);
-
+void dumpGroupDesc(struct ext2_group_desc);
 
 void read_ext2() {
     FILE *fp;
@@ -21,10 +21,9 @@ void read_ext2() {
     fread(&ext2_sb, sizeof(struct ext2_super_block), 1, fp);
     dumpSuperBlock(ext2_sb);
 
-    fread(&ext2_gd, sizeof(struct ext2_group_desc), 1, fp);
-    printf("%20s  :  %-d\n","bg_inode_table:",ext2_gd.bg_inode_table);
-
     // group desc
+    fread(&ext2_gd, sizeof(struct ext2_group_desc), 1, fp);
+    dumpGroupDesc(ext2_gd);
 
     fclose(fp);
 }
@@ -57,12 +56,29 @@ void dumpSuperBlock(struct ext2_super_block sb) {
     printf("s_block_group_nr : %d\n", sb.s_block_group_nr);
     printf("s_last_mounted : %s\n", sb.s_last_mounted);
 
-    printf("s_uuid : ");
-    dumpUUID(sb.s_uuid);
+    dumpUUID("s_uuid : ", sb.s_uuid);
     printf("-------------------------------------------------\n");
 }
 
-void dumpUUID(__u8* uuid) {
+void dumpGroupDesc(struct ext2_group_desc gd) {
+    printf("-------------------------------------------------\n");
+    printf("                ext2 group desc\n");
+    printf("-------------------------------------------------\n");
+
+    printf("bg_block_bitmap : %-d\n", gd.bg_block_bitmap);
+    printf("bg_inode_bitmap : %-d\n", gd.bg_inode_bitmap);
+    printf("bg_inode_table : %-d\n", gd.bg_inode_table);
+    printf("bg_free_blocks_count : %-d\n", gd.bg_free_blocks_count);
+    printf("bg_free_inodes_count : %-d\n", gd.bg_free_inodes_count);
+    printf("bg_used_dirs_count : %-d\n", gd.bg_used_dirs_count);
+    printf("bg_flags : %-d\n", gd.bg_flags);
+    printf("bg_itable_unused : %-d\n", gd.bg_itable_unused);
+    printf("bg_checksum : %-d\n", gd.bg_checksum);
+    printf("-------------------------------------------------\n");
+}
+
+void dumpUUID(const char *name, __u8* uuid) {
+    printf(name);
     for (int i=0; i<16;) {
         printf("%02x", ((unsigned char*) uuid)[i++]);
         if (i == 4 || i == 6 || i==8 || i==10) {
